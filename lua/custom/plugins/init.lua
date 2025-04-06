f0a02ed36e69adca99db5b946a86cb2f731035fa
felipe magrassi <felipe.1magrassi@gmail.com>
2025-04-05 22:58:34 -0300

    add oil nvim and setup open command

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,6 +13,10 @@ vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('n', '<leader>x', '<cmd>:qa!<cr>', { desc = 'Quit' })
 vim.keymap.set('n', '<leader>sc', require('telescope.builtin').commands, { desc = '[S]earch [C]ommands' })
 
+vim.keymap.set('n', '<leader>oc', '<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/custom/plugins/init.lua<CR>',
+  { desc = 'Open Config' })
+vim.keymap.set('n', '<leader>ot', '<cmd>e ~/todo.txt<CR>', { desc = 'Open Todo' })
+
 vim.opt.spell = false
 vim.opt.spelllang = 'pt_br,en_us'
 
@@ -30,7 +34,7 @@ vim.opt.scrolloff = 8
 vim.opt.smartcase = true
 
 require('which-key').add {
-  { '<leader>j', group = '[J]ump' },
+  { '<leader>o', group = '[O]pen' },
   { '<leader>d', group = '[C-t]est' },
   { '<leader>g', group = '[G]it' },
   { '<leader>m', group = '[M]agestic Scripts' },
@@ -101,6 +105,31 @@ return {
     'https://github.com/rafamadriz/friendly-snippets',
   },
   {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    keys = {
+      { '<leader>e', ':Oil<cr>', {
+        noremap = true,
+        silent = true,
+        desc = 'Oil',
+      } },
+    },
+    config = function()
+      require('oil').setup {
+        view_options = {
+          show_hidden = true,
+        },
+      }
+    end,
+  },
+  {
     'rafamadriz/friendly-snippets',
     config = function()
       require('luasnip.loaders.from_vscode').lazy_load()
@@ -122,13 +151,6 @@ return {
   },
   { 'tpope/vim-dispatch' },
   { 'rebelot/kanagawa.nvim' },
-  {
-    'tpope/vim-rails',
-    ft = { 'ruby' },
-    keys = {
-      { '<leader>ja', [[:execute "e " . eval('rails#buffer().alternate()')<cr>]], desc = 'Jump rails alternate' },
-    },
-  },
   {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
@@ -355,64 +377,64 @@ return {
     priority = 1000,
   },
   {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
-  opts = {
-    -- add any opts here
-    -- for example
-    provider = "claude",
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-      temperature = 0,
-      max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+      -- add any opts here
+      -- for example
+      provider = 'claude',
+      openai = {
+        endpoint = 'https://api.openai.com/v1',
+        model = 'gpt-4o',  -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000,   -- Timeout in milliseconds, increase this for reasoning models
+        temperature = 0,
+        max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      },
     },
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'echasnovski/mini.pick',         -- for file_selector provider mini.pick
+      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      'hrsh7th/nvim-cmp',              -- autocompletion for avante commands and mentions
+      'ibhagwan/fzf-lua',              -- for file_selector provider fzf
+      'nvim-tree/nvim-web-devicons',   -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua',        -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
           },
-          -- required for Windows users
-          use_absolute_path = true,
         },
       },
-    },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
       },
-      ft = { "markdown", "Avante" },
     },
   },
-},
   {
     'sainnhe/everforest',
     lazy = false,
